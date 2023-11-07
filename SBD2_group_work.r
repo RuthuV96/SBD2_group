3696937079edd354c70c5e0d7b03a906f928b600
RuthuV96 <110454226+RuthuV96@users.noreply.github.com>
2023-11-07 14:13:03 +0100

    Update SBD2_group_work.r

--- SBD2_group_work.r
+++ SBD2_group_work.r
@@ -2,7 +2,7 @@
 libraries = c("readr", "ggplot2","Boruta", "ROCR", "caret", "pROC", "dplyr", "ROSE", "corrplot", "DescTools", "ggpubr", "tidyverse", "RColorBrewer", "ggcorrplot", "PerformanceAnalytics", "corrr", "networkD3", "reshape", "knitr", "dlookr")
 
 # Load the dataset
-btc_data <- read.csv("loan_sample_11.csv")
+data <- read.csv("loan_sample_11.csv")
 
 ## Exercise 1
 
